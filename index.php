e6151101ba49a02ac8754aabec61dcf7c7ba059e
Brent Garner <bgarner@gmail.com>
2016-10-07 14:43:23 -0600

    setting up the template

---
+++ index.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+	<meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/jumbotron-narrow.css">
+        <style>
+            .thumbnail {
+                padding:0;
+            }
+
+        </style>
+    </head>
+
+    <body>
+            <div class="container">
+
+            <form>
+              <div class="jumbotron">
+                <h1>Meet With Duncan</h1>
+                <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+  <!--               -->
+
+                
+
+                <form>
+                      <div class="form-group">
+
+                        <label for="exampleInputEmail1" class="pull-left">First Name</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="First Name">
+                        <br />
+                        <label for="exampleInputEmail1" class="pull-left">Last Name</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Last Name">
+                        <br />
+                        <label for="exampleInputEmail1" class="pull-left">Department</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Department">
+                        <br />
+                        <label for="exampleInputEmail1" class="pull-left">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                      </div>
+
+                 </form>
+              </div>
+
+              <div class="row marketing">
+                <center>
+                <h2>Select a Time</h2>
+                </center>
+                <div class="row">
+                            <div class="col-sm-4">
+                                <div class="thumbnail">
+                                    <div class="caption">
+                                        <center>
+                                            <h2>10 seats</h2>
+                                            <h4 class="">Tuesday, Oct 10 <br />4:00pm - 5:00pm</h4>
+                                            <p class="">Atmosphere Boardroom</p> 
+                                        </center>
+                                            <a href="#" class="btn btn-default btn-xs pull-right" role="button"><i class="glyphicon glyphicon-edit"></i></a>  <a href="#" class="btn btn-default btn-xs" role="button">More Info</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-4">
+                                <div class="thumbnail">
+                                    <div class="caption">
+                                        <center>
+                                            <h2>10 seats</h2>
+                                            <h4 class="">Tuesday, Oct 10 <br />4:00pm - 5:00pm</h4>
+                                            <p class="">Atmosphere Boardroom</p> 
+                                        </center>
+                                            <a href="#" class="btn btn-default btn-xs pull-right" role="button"><i class="glyphicon glyphicon-edit"></i></a>  <a href="#" class="btn btn-default btn-xs" role="button">More Info</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-4">
+                                <div class="thumbnail">
+                                    <div class="caption">
+                                        <center>
+                                            <h2>10 seats</h2>
+                                            <h4 class="">Tuesday, Oct 10 <br />4:00pm - 5:00pm</h4>
+                                            <p class="">Atmosphere Boardroom</p> 
+                                        </center>
+                                            <a href="#" class="btn btn-default btn-xs pull-right" role="button"><i class="glyphicon glyphicon-edit"></i></a>  <a href="#" class="btn btn-default btn-xs" role="button">More Info</a>
+                                    </div>
+                                </div>
+                            </div>                                                       
+
+                </div>
+
+                <div class="row">
+                            <div class="col-sm-4">
+                                <div class="thumbnail">
+                                    <div class="caption">
+                                        <center>
+                                            <h2>10 seats</h2>
+                                            <h4 class="">Tuesday, Oct 10 <br />4:00pm - 5:00pm</h4>
+                                            <p class="">Atmosphere Boardroom</p> 
+                                        </center>
+                                            <a href="#" class="btn btn-default btn-xs pull-right" role="button"><i class="glyphicon glyphicon-edit"></i></a>  <a href="#" class="btn btn-default btn-xs" role="button">More Info</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-4">
+                                <div class="thumbnail">
+                                    <div class="caption">
+                                        <center>
+                                            <h2>10 seats</h2>
+                                            <h4 class="">Tuesday, Oct 10 <br />4:00pm - 5:00pm</h4>
+                                            <p class="">Atmosphere Boardroom</p> 
+                                        </center>
+                                            <a href="#" class="btn btn-default btn-xs pull-right" role="button"><i class="glyphicon glyphicon-edit"></i></a>  <a href="#" class="btn btn-default btn-xs" role="button">More Info</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-4">
+                                <div class="thumbnail">
+                                    <div class="caption">
+                                        <center>
+                                            <h2>10 seats</h2>
+                                            <h4 class="">Tuesday, Oct 10 <br />4:00pm - 5:00pm</h4>
+                                            <p class="">Atmosphere Boardroom</p> 
+                                        </center>
+                                            <a href="#" class="btn btn-default btn-xs pull-right" role="button"><i class="glyphicon glyphicon-edit"></i></a>  <a href="#" class="btn btn-default btn-xs" role="button">More Info</a>
+                                    </div>
+                                </div>
+                            </div>                                                       
+
+                </div>
+
+
+                <div class="row">
+                            <div class="col-sm-4">
+                                <div class="thumbnail">
+                                    <div class="caption">
+                                        <center>
+                                            <h2>10 seats</h2>
+                                            <h4 class="">Tuesday, Oct 10 <br />4:00pm - 5:00pm</h4>
+                                            <p class="">Atmosphere Boardroom</p> 
+                                        </center>
+                                            <a href="#" class="btn btn-default btn-xs pull-right" role="button"><i class="glyphicon glyphicon-edit"></i></a>  <a href="#" class="btn btn-default btn-xs" role="button">More Info</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-4">
+                                <div class="thumbnail">
+                                    <div class="caption">
+                                        <center>
+                                            <h2>10 seats</h2>
+                                            <h4 class="">Tuesday, Oct 10 <br />4:00pm - 5:00pm</h4>
+                                            <p class="">Atmosphere Boardroom</p> 
+                                        </center>
+                                            <a href="#" class="btn btn-default btn-xs pull-right" role="button"><i class="glyphicon glyphicon-edit"></i></a>  <a href="#" class="btn btn-default btn-xs" role="button">More Info</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-4">
+                                <div class="thumbnail">
+                                    <div class="caption">
+                                        <center>
+                                            <h2>10 seats</h2>
+                                            <h4 class="">Tuesday, Oct 10 <br />4:00pm - 5:00pm</h4>
+                                            <p class="">Atmosphere Boardroom</p> 
+                                        </center>
+                                            <a href="#" class="btn btn-default btn-xs pull-right" role="button"><i class="glyphicon glyphicon-edit"></i></a>  <a href="#" class="btn btn-default btn-xs" role="button">More Info</a>
+                                    </div>
+                                </div>
+                            </div>                                                       
+
+                </div>                                
+
+
+              </div>
+
+
+              <p><a class="btn btn-lg btn-success" href="#" role="button">Send Registration</a></p> 
+              
+            </div>
+
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+        <script src="js/bootstrap.js"></script>
+    </body>
+
+</html>
